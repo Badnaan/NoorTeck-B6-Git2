945042411eefcb26261a3562f7c93b04d9df959f
Gamer1650 <Gamer1650@GAMER1650>
2022-05-05 20:34:37 -0400

    added the 4th method

--- src/test/java/git2/DemoTwo.java
+++ src/test/java/git2/DemoTwo.java
@@ -5,6 +5,10 @@ public class DemoTwo {
 	public static void main(String[] args) {
 		
 		System.out.println("Git demo 2...");
+		
+		
+		
+		sub(2, 3);
 	}
 	
 	public void add (int a, int b)
@@ -14,6 +18,11 @@ public class DemoTwo {
 	public static void sub (int z, int xc )
 	{
 		System.out.println(z - xc);
+	}
+	public void chicken (int egg , int chicken)
+	{
+		
+		System.out.println("Who came first? " + egg + chicken);
 	}
 
 }
