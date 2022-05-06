0b2aa9e3729833f34e13ea90fed6978eacf8b8b8
Gamer1650 <Gamer1650@GAMER1650>
2022-05-05 21:21:19 -0400

    changes being commited

--- src/test/java/git2/DemoFour.java
+++ src/test/java/git2/DemoFour.java
@@ -5,5 +5,11 @@ public class DemoFour {
 	public static void main(String[] args) {
 		System.out.println("Demo FOUR");
 	}
+	public static void div (int a , int b)
+	{
+		int result = a/b;
+		System.out.println(result);
+	}
+	
 
 }
